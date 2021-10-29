81952d9d6be979c64f92e72762f4ca50561007c7
Philip Offtermatt <philip.offtermatt@usherbrooke.ca>
2021-10-29 09:48:59 -0400

    Increase default timeout for statistics

--- artifact/benchmark/compute_net_statistics.py
+++ artifact/benchmark/compute_net_statistics.py
@@ -10,7 +10,7 @@ import pandas
 from typing import Dict
 import argparse
 
-timeout_time = 60
+timeout_time = 600
 
 
 def compute_statistics(sample_name, lola_file, formula_file, output_file, prune, monotonicity, first):
