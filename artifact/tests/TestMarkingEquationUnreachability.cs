fa7adfa1a051c62f5d1731792c30440e54e1e298
Philip Offtermatt <philip.offtermatt@usherbrooke.ca>
2021-11-08 13:53:07 -0500

    Add tests for unreachability implementation

---
+++ artifact/tests/TestMarkingEquationUnreachability.cs
@@ -0,0 +1,38 @@
+using Xunit;
+using System;
+using System.Collections.Generic;
+using Petri;
+using Xunit.Abstractions;
+using PetriTool;
+using Soundness;
+using System.Linq;
+
+namespace Testing
+{
+
+
+    public class TestMarkingEquationUnreachability
+    {
+
+        private readonly ITestOutputHelper output;
+
+        public TestMarkingEquationUnreachability(ITestOutputHelper output)
+        {
+            this.output = output;
+        }
+
+        [Theory]
+        [InlineData("unreachability/n1.lola", "unreachability/n1.formula", false)]
+        public void TestUnreachability(string netfilepath, string formulafilepath, bool reachableExpected)
+        {
+            FullParser parser = ParserPicker.ChooseFullParser(netfilepath, formulafilepath);
+
+            (PetriNet net, Marking initialMarking) = parser.ReadNet(Utils.GetPathForTestfile(netfilepath));
+            Marking target = parser.ReadFormula(Utils.GetPathForTestfile(formulafilepath)).First().Marking;
+
+            Dictionary<Place, double> result = GurobiHeuristics.CheckUnreachability(net.Places, net.Transitions, initialMarking, target);
+
+            Assert.Equal(result == null, reachableExpected);
+        }
+    }
+}
