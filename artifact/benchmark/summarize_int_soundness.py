d9a179acf1efcc0ce45d9f03b3b787f305992ab1
Philip Offtermatt <philip.offtermatt@usherbrooke.ca>
2021-11-10 08:56:34 -0500

    Prettify script, print more info

--- artifact/benchmark/summarize_int_soundness.py
+++ artifact/benchmark/summarize_int_soundness.py
@@ -6,14 +6,36 @@ import pandas
 
 def print_statistics(entries):
     print("Printing statistics...")
+    print("----------------------------------------")
+
     print(f"Total entries: {len(entries)}")
+    print("----------------------------------------")
 
     error_free_entries = [entry for entry in entries if "error" not in entry]
     print(f"Error-free entries: {len(error_free_entries)}")
+    print("----------------------------------------")
+
 
     workflow_entries = [
         entry for entry in error_free_entries if entry["isWorkflowNet"]]
     print(f"Workflow nets: {len(workflow_entries)}")
+    print("----------------------------------------")
+
+    integer_bounded_entries = [entry for entry in workflow_entries if entry["wfIntegerBoundednessCounterexample"] == "None"]
+    print(f"Integer-bounded entries: {len(integer_bounded_entries)}")
+    print("----------------------------------------")
+
+    integer_sound_nets = pandas.Series([entry["timeForIntegerSoundness"]/1000 for entry in integer_bounded_entries if entry["isIntegerSound"] == True])
+    print(f"Times for integer-sound nets:")
+    print(integer_sound_nets.describe())
+    print("----------------------------------------")
+
+    integer_unsound_nets = pandas.Series([entry["timeForIntegerSoundness"]/1000 for entry in integer_bounded_entries if entry["isIntegerSound"] == False])
+    print(f"Times for integer-unsound nets:")
+    print(integer_unsound_nets.describe())
+    print("----------------------------------------")
+
+
 
     # totalTimes = pandas.Series(numpy.array([entry["timeInQuery"]/1000 for entry in entries if "error" not in entry]))
     # soundTimes = pandas.Series(numpy.array([entry["timeInQuery"]/1000 for entry in entries if "error" not in entry and entry["isContinuousSound"]]))
