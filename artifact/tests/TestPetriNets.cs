feb05deb0973dc793a3632ba405488c0502a7666
Philip Offtermatt <philip.offtermatt@usherbrooke.ca>
2021-11-23 08:31:11 -0500

    Add more testcases for removing uncoverable transitions

--- artifact/tests/TestPetriNets.cs
+++ artifact/tests/TestPetriNets.cs
@@ -60,18 +60,27 @@ namespace Testing
 
         [Theory]
         [InlineData("remove-uncoverable-transitions/1.lola", "r1 r2 r3")]
+        [InlineData("remove-uncoverable-transitions/2.lola", null)]
+        [InlineData("remove-uncoverable-transitions/3.lola", null)]
+        [InlineData("remove-uncoverable-transitions/4.lola", "r1")]
         public void TestRemoveUncoverableTransitions(string net_filepath, string space_separated_expected_removed_transitionnames)
         {
             NetParser parser = new LolaParser();
             (PetriNet net, Marking initialMarking) = parser.ReadNet(Utils.GetPathForTestfile(net_filepath));
             PetriNet modifiedNet = UtilityEntrypoints.RemoveUncoverableTransitions(net, initialMarking);
 
-            string[] expectedRemovedTransitionNames = space_separated_expected_removed_transitionnames.Split(" ");
-            Array.Sort(expectedRemovedTransitionNames);
-
             string[] actualRemovedTransitionNames =
                 net.Transitions.Where(transition => !modifiedNet.Transitions.Contains(transition)).Select(transition => transition.Name).ToArray();
+
+            if (space_separated_expected_removed_transitionnames == null)
+            {
+                Assert.Equal(0, actualRemovedTransitionNames.Count());
+                return;
+            }
+
+            string[] expectedRemovedTransitionNames = space_separated_expected_removed_transitionnames.Split(" ");
             Array.Sort(actualRemovedTransitionNames);
+            Array.Sort(expectedRemovedTransitionNames);
 
             Assert.Equal(expectedRemovedTransitionNames, actualRemovedTransitionNames);
         }
