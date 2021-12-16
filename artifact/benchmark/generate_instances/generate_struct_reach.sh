84c4dbed0ee2e948e61a2824f63ee2b4ea8c769b
Philip Offtermatt <philip.offtermatt@usherbrooke.ca>
2021-12-16 09:54:13 -0500

    Adapt script for generating structural reach instances: Fastforward needs reachability formula input

--- artifact/benchmark/generate_instances/generate_struct_reach.sh
+++ artifact/benchmark/generate_instances/generate_struct_reach.sh
@@ -6,6 +6,8 @@ for FILE in ../nets/workflows/synthetic/structural-soundness/lola/*/*_1-check.lo
     FILENAME=${FILENAME%.lola}
     TARGET_ROOT="../nets/workflows/synthetic/structural-reachability"
     mkdir -p "${TARGET_ROOT}/fastforward/${FILENAME%/*}"
-    cp ${FILE} "${TARGET_ROOT}/fastforward/${FILENAME}.lola"
+    # do not need to perform structural reachability reduction for fastforward, since struct. reach is solved by continuous reach
+    # that's why mode is Reachability
+    dotnet ../fastforward/fastforward.dll translate-wf ${FILE} -m Reachability -f Lola -o "${TARGET_ROOT}/fastforward/${FILENAME}"
     dotnet ../fastforward/fastforward.dll translate-wf ${FILE} -m StructuralReachability -f Lola -o "${TARGET_ROOT}/lola/${FILENAME}"
     done
