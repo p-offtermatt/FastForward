4ec29b3f69f107d0c5b8b411afedaa9585b9dee3
Philip Offtermatt <philip.offtermatt@usherbrooke.ca>
2021-12-16 09:53:28 -0500

    Adapt script to check structural quasi-soundness

--- artifact/benchmark/benchmark_structural-quasi-soundness.py
+++ artifact/benchmark/benchmark_structural-quasi-soundness.py
@@ -9,11 +9,25 @@ from pathlib import Path
 
 timeout_time = 120
 
+def CheckFormulaFileExists(net_filepath):
+    formula_filepath = GetFormulaFileForNet(net_filepath)
+    if not os.path.isfile(formula_filepath):
+        raise FileNotFoundError("Netfile " + net_filepath + " does not have an associated .formula file! Make sure the file is in the same folder and has the same name, except for a .formula extension: " + formula_filepath)
+
+def GetFormulaFileForNet(net_filepath):
+    root,ext = os.path.splitext(net_filepath)
+    assert(ext == ".lola")
+    return root + ".formula"
+
+
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument('benchmark_dir', type=str,
                         help="The directory containing the lola and fastforward subfolders, which should in turn contain the input files, potentially again in subfolders. Subfolders are assumed to be mirrored among the lola and fastforward subfolders.")
-    parser.add_argument("-o", "--outputfile", type=str, required=True)
+    parser.add_argument("-o", "--outputfile", type=str, required=True, 
+        help="""The base name for the output file. Should not contain the extension, which will be chosen automatically. Also, one output file will be 
+        produced per subdirectory in the fastforward and lola subfolders. For example, if there is a folder lola/unsound,
+        then one output file will be called {outputfile}_unsound.json""")
 
     args = parser.parse_args()
 
@@ -25,6 +39,7 @@ if __name__ == "__main__":
     folder_name = Path(folder_name).absolute()
     print(folder_name)
     
+    # get instances in lola and fastforward subdirectories
     lola_folder = folder_name.joinpath("lola")
     ff_folder = folder_name.joinpath("fastforward")
     print(f"Looking for LoLA inputs in {lola_folder}")
@@ -48,7 +63,8 @@ if __name__ == "__main__":
         for file in files:
             if file.endswith(".lola"):
                 ff_files[reldir] += [file]
-
+    
+    # ensure that both directories contain the same instances, in this case .lola files
     if(ff_files != lola_files):
         print("Input instances for lola and fastforward are not the same.")
         for dir,entries in lola_files.items():
@@ -67,63 +83,56 @@ if __name__ == "__main__":
             difference = set(entries) - set(lola_files[dir])
             if len(difference) > 0:
                 print("Extra files for FastForward in " + dir + ": " + str(difference))
-            
+    
+    # ff_files and lola_files are identical; to avoid naming confusion, assign new variable
+    files = lola_files
 
-    # with open(output_filepath, 'w+') as output_file:
-    #     output_file.write("[")
-    #     output_file.flush()
-    #     first = True
-    #     for entry in os.scandir(folder_name):
-    #         path = entry.path
-    #         print("----" + path + "----")
-    #         if path.endswith(".lola"):
-    #             ending = ".lola"
-    #         elif path.endswith(".xml"):
-    #             ending = ".xml"
-    #         else:
-    #             continue
+    # ensure all files have an associated formula file, e.g. for file.lola, there should exist file.formula in the same subfolder
+    for dir, entries in files.items():
+        for entry in entries:
+            net_filepath = os.path.join(ff_folder, dir, entry)
+            CheckFormulaFileExists(net_filepath)
 
-    #         if not first:
-    #             output_file.write(",\n")
-    #         first = False
+            net_filepath = os.path.join(lola_folder, dir, entry)
+            CheckFormulaFileExists(net_filepath)
 
-    #         path_prefix = path[:-len(ending)]
-    #         folder_prefix, filename = path_prefix.rsplit("/", 1)
+    for dir, entries in files.items():
+        # write result file for this directory of instances
+        with open(output_filepath, 'w') as output_file:
+            output_file.write("[")
+            output_file.flush()
+            first = True
+            for entry in entries:
+                print(f"---- {dir}/{entry} ----")
 
-    #         command = f"dotnet fastforward/fastforward.dll soundness-reverseTransitions {path_prefix}{ending} -k 1"
-    #         process = Popen(command.split(" "), stdout=PIPE,
-    #                         stderr=PIPE, preexec_fn=benchmark_utils.limit_virtual_memory)
-    #         try:
-    #             print(command)
-    #             execution_time = time.time()
-    #             result, stderr = process.communicate(timeout=timeout_time)
-    #             result_obj = json.loads(result)
-    #         except CalledProcessError:
-    #             execution_time = time.time() - execution_time
-    #             process.kill()
-    #             result, stderr = process.communicate()
 
-    #             result_obj = {"error": stderr.decode(
-    #                 "utf-8").replace("\"", "'")}
-    #         except TimeoutExpired:
-    #             execution_time = time.time() - execution_time
-    #             result_obj = {"error": "timeout"}
-    #             process.kill()
-    #             result, stderr = process.communicate(timeout=timeout_time)
+                benchmark_suite = dir
+                instance_name = entry
 
-    #             print("Timeout!")
-    #         except json.JSONDecodeError as e:
-    #             process.kill()
-    #             print("Encountered an error:")
-    #             print(e.msg)
-    #             print(stderr.decode("utf-8"))
-    #             print(result.decode("utf-8"))
-    #             result_obj = {"error": repr(
-    #                 e) + ", " + stderr.decode("utf-8".replace("\"", "'"))}
-    #         result_obj["file"] = path
+                ff_netpath = os.path.join(ff_folder, dir, entry)
+                ff_formulapath = GetFormulaFileForNet(ff_netpath)
 
-    #         output_file.write(json.dumps(result_obj))
-    #         output_file.flush()
+                
+                ff_result = benchmark_utils.call_fastforward("calculate-heuristic", ff_netpath, ff_formulapath, pruning=False,
+                    extra_options="-h qReachability", timeout_time=timeout_time)
+                ff_result["sampleName"] = entry
+                ff_result["methodName"] = "continuous"
 
-    #     output_file.write("]")
-    #     output_file.flush()
+                if not first:
+                    output_file.write(",\n")
+                first = False
+                output_file.write(json.dumps(ff_result))
+                output_file.flush()
+
+                lola_filepath = os.path.join(lola_folder, dir, entry)
+                lola_formulapath = GetFormulaFileForNet(lola_filepath)
+
+                lola_result = benchmark_utils.call_lola(lola_filepath, lola_formulapath, timeout_time)
+                lola_result["sampleName"] = entry
+                lola_result["methodName"] = "reachability"
+
+                output_file.write(",\n")
+                output_file.write(json.dumps(lola_result))
+                output_file.flush()
+            output_file.write("]")
+            output_file.flush()
