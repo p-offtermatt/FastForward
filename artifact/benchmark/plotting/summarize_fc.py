c5619d29aa3c13052f22aa65cbeaf84140a936bd
Philip Offtermatt <philip.offtermatt@usherbrooke.ca>
2021-12-20 15:43:53 -0500

    Add getting reason from woflan diagnosis

--- artifact/benchmark/plotting/summarize_fc.py
+++ artifact/benchmark/plotting/summarize_fc.py
@@ -5,6 +5,24 @@ import plotting_utils as utils
 import matplotlib.pyplot as plt
 
 
+def get_woflan_reason(entry):
+    if "diagnosisResult" not in entry:
+        return "dnf"
+    
+    diagnosis = entry["diagnosisResult"]
+
+    if "not bounded" in diagnosis:
+        return "unbounded"
+    
+    if "dead transitions" in diagnosis:
+        return "dead"
+
+    if "not live" in diagnosis:
+        return "nonlive"
+
+    assert False, diagnosis
+    
+
 def print_statistics(entries, name):
     print("Printing statistics...")
     print("----------------------------------------")
@@ -36,7 +54,9 @@ def print_statistics(entries, name):
     
     grouped_tuples = [(name, entry) for name, entry in grouped_entries.items()]
 
-    names_to_conti_entires = utils.group_by_name(continuous_reach_entries)
+    names_to_conti_entries = utils.group_by_name(continuous_reach_entries)
+    names_to_woflan_entries = utils.group_by_name(woflan_entries)
+
 
     
     print("----------------Largest 10 entries-------------------------")
@@ -45,8 +65,10 @@ def print_statistics(entries, name):
         sample_name = entry[0]
         row = [sample_name]
         # add info about net: num of places and transitions
-        row += [names_to_conti_entires[sample_name][0]["numberOfPlaces"]]
-        row += [names_to_conti_entires[sample_name][0]["numberOfTransitions"]]
+        row += [names_to_conti_entries[sample_name][0]["numberOfPlaces"]]
+        row += [names_to_conti_entries[sample_name][0]["numberOfTransitions"]]
+
+        # row += [get_woflan_reason(names_to_woflan_entries[sample_name][0])]
 
         # add results for tools
         row += utils.get_time_from_entries(entry[1])
@@ -55,8 +77,8 @@ def print_statistics(entries, name):
 
     headers = ["sampleName", "numPlaces", "numTransitions"] + [entry["methodName"] for entry in grouped_tuples[0][1]]
 
-    data.sort(key=lambda entry: sum(entry[1:]), reverse=True)
-    print(tabulate(data[:10], headers, tablefmt="latex", floatfmt=".1f"))
+    data.sort(key=lambda entry: sum(entry[-3:]), reverse=True)
+    print(tabulate(data[:20], headers, tablefmt="latex", floatfmt=".1f"))
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
