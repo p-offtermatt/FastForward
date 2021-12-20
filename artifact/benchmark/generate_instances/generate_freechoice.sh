8c6bc71a348f06fa4126a989ab9b8f51edf41cb0
Philip Offtermatt <philip.offtermatt@usherbrooke.ca>
2021-12-20 09:28:21 -0500

    Remove extension when generating instances, since it autogenerates

---
+++ artifact/benchmark/generate_instances/generate_freechoice.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+base_path="../nets/workflows/free-choice/"
+mkdir -p "${base_path}woflan"
+mkdir -p "${base_path}continuous"
+mkdir -p "${base_path}lola"
+
+for FILE in ../nets/workflows/workflows_transformed/IBM/*.lola;
+    do
+    echo "${FILE}"
+    FILENAME=${FILE#../nets/workflows/workflows_transformed/IBM/}
+    FILENAME_NO_EXTENSION=${FILENAME%.lola}
+
+    TARGET="${base_path}continuous/"${FILENAME}
+    cp ${FILE} ${TARGET}
+    python3 reduce_net.py ${TARGET}
+    dotnet ../fastforward/fastforward.dll translate-wf ${TARGET} -f PNML -o "${base_path}woflan/${FILENAME_NO_EXTENSION}" -m Reachability
+    dotnet ../fastforward/fastforward.dll translate-wf ${TARGET} -f Lola -o "${base_path}lola/${FILENAME_NO_EXTENSION}" -m Soundness
+    done
