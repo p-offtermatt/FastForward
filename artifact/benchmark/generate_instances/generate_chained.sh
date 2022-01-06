b2218f7022dcb31e9bd490cb46787030248e16e7
Philip Offtermatt <philip.offtermatt@usherbrooke.ca>
2022-01-06 09:36:36 -0500

    change chained generation to generate for different chain nums

--- artifact/benchmark/generate_instances/generate_chained.sh
+++ artifact/benchmark/generate_instances/generate_chained.sh
@@ -1,28 +1,33 @@
 #!/bin/bash
-base_path=../nets/workflows/chained_workflows/
 
+REPEATS=10
 rm -r ../nets/workflows/chained_workflows*
-python3 chain_nets.py ../nets/workflows/fc-safety ../nets/workflows/chained_workflows_unreduced/
 
-mkdir -p ../nets/workflows/chained_workflows_templates/
-cp ../nets/workflows/chained_workflows_unreduced/* ../nets/workflows/chained_workflows_templates/
-mkdir -p "${base_path}woflan/"
-mkdir -p "${base_path}lola/"
-mkdir -p "${base_path}continuous/"
-
-for FILE in ../nets/workflows/chained_workflows_templates/*.lola;
+for ((chain=1;chain<=201;chain=chain+20))
     do
-    echo ${FILE}
-    python3 reduce_net.py ${FILE}
+    base_path=../nets/workflows/chained_workflows_${chain}/
+
+    python3 chain_nets.py ../nets/workflows/fc-safety ../nets/workflows/chained_workflows_unreduced_${chain}/ $REPEATS $chain
+
+    mkdir -p ../nets/workflows/chained_workflows_templates_${chain}/
+    cp ../nets/workflows/chained_workflows_unreduced_${chain}/* ../nets/workflows/chained_workflows_templates_${chain}/
+    mkdir -p "${base_path}woflan/"
+    mkdir -p "${base_path}lola/"
+    mkdir -p "${base_path}continuous/"
+
+    for FILE in ../nets/workflows/chained_workflows_templates_${chain}/*.lola;
+        do
+        echo ${FILE}
+        sleep 2c
+        python3 reduce_net.py ${FILE}
 
 
-    FILENAME=${FILE##*/}
-    FILENAME_NO_EXTENSION=${FILENAME%.lola}
+        FILENAME=${FILE##*/}
+        FILENAME_NO_EXTENSION=${FILENAME%.lola}
 
-    dotnet ../fastforward/fastforward.dll translate-wf ${FILE} -f PNML -o "${base_path}woflan/${FILENAME_NO_EXTENSION}" -m Reachability
-    dotnet ../fastforward/fastforward.dll translate-wf ${FILE} -f Lola -o "${base_path}lola/${FILENAME_NO_EXTENSION}" -m Soundness
-    dotnet ../fastforward/fastforward.dll translate-wf ${FILE} -f Lola -o "${base_path}continuous/${FILENAME_NO_EXTENSION}" -m Soundness
+        dotnet ../fastforward/fastforward.dll translate-wf ${FILE} -f PNML -o "${base_path}woflan/${FILENAME_NO_EXTENSION}" -m Reachability
+        dotnet ../fastforward/fastforward.dll translate-wf ${FILE} -f Lola -o "${base_path}lola/${FILENAME_NO_EXTENSION}" -m Soundness
+        dotnet ../fastforward/fastforward.dll translate-wf ${FILE} -f Lola -o "${base_path}continuous/${FILENAME_NO_EXTENSION}" -m Soundness
 
-
-
+        done
     done
