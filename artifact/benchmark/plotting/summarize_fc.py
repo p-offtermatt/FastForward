b2663c0c384fc0862c5015b683880ac9739201cc
Philip Offtermatt <philip.offtermatt@usherbrooke.ca>
2022-01-06 08:18:01 -0500

    Improve summarizing fc

--- artifact/benchmark/plotting/summarize_fc.py
+++ artifact/benchmark/plotting/summarize_fc.py
@@ -3,6 +3,7 @@ import argparse
 from tabulate import tabulate
 import plotting_utils as utils
 import matplotlib.pyplot as plt
+import numpy as np
 
 
 def get_woflan_reason(entry):
@@ -57,9 +58,8 @@ def print_statistics(entries, name):
     names_to_conti_entries = utils.group_by_name(continuous_reach_entries)
     names_to_woflan_entries = utils.group_by_name(woflan_entries)
 
-
-    
-    print("----------------Largest 10 entries-------------------------")
+    num_entries = 50
+    print(f"----------------Largest {num_entries} entries-------------------------")
     data = []
     for entry in grouped_tuples:
         sample_name = entry[0]
@@ -67,18 +67,38 @@ def print_statistics(entries, name):
         # add info about net: num of places and transitions
         row += [names_to_conti_entries[sample_name][0]["numberOfPlaces"]]
         row += [names_to_conti_entries[sample_name][0]["numberOfTransitions"]]
+        row += ["yes" if names_to_conti_entries[sample_name][0]["isZBounded"] == True else "no"]
+        # row += [("o_star" in names_to_woflan_entries[sample_name][0]["diagnosisResult"]) if "diagnosisResult" in names_to_woflan_entries[sample_name][0] else "dnf"]
 
         # row += [get_woflan_reason(names_to_woflan_entries[sample_name][0])]
 
+        row += [names_to_conti_entries[sample_name][0]["timeForZBoundedness"]]
+        row += [names_to_conti_entries[sample_name][0]["timeForContinuousSoundness"]]
         # add results for tools
         row += utils.get_time_from_entries(entry[1])
 
         data.append(row)
 
-    headers = ["sampleName", "numPlaces", "numTransitions"] + [entry["methodName"] for entry in grouped_tuples[0][1]]
+    headers = ["sampleName", "numPlaces", "numTransitions", "time for z-boundedness", "time for continuous soundness"] + [entry["methodName"] for entry in grouped_tuples[0][1]]
 
     data.sort(key=lambda entry: sum(entry[-3:]), reverse=True)
-    print(tabulate(data[:20], headers, tablefmt="latex", floatfmt=".1f"))
+
+    data = data[:num_entries]
+
+    zunbounded = [list(entry) for entry in data if entry[3] == "no"]
+    zbounded = [list(entry) for entry in data if entry[3] == "yes"]
+
+    data = [["z-unbounded"]] + zunbounded + [["midrule"], ["continuous-unsound"]] + zbounded
+
+    for i, entry in enumerate(data):
+        if len(entry) >= 4:
+            del entry[3]
+
+    print(tabulate(data, headers, floatfmt=".1f", tablefmt="plain").replace("120000.0", "dnf"))
+    print(np.mean([float(entry[-2]) for entry in data if len(entry) > 3]))
+    print(np.mean([float(entry[-1]) for entry in data if len(entry) > 3]))
+
+
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
