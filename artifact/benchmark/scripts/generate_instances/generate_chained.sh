3b11f3506677d03e7be3d73c5a2fcf9bae901d59
Philip Offtermatt <philip.offtermatt@usherbrooke.ca>
2022-02-23 16:02:44 -0500

    Reorganize benchmark instances

--- artifact/benchmark/scripts/generate_instances/generate_chained.sh
+++ artifact/benchmark/scripts/generate_instances/generate_chained.sh
@@ -1,21 +1,21 @@
 #!/bin/bash
 
 REPEATS=20
-rm -r ../nets/workflows/chained_workflows*
+rm -r ../../instances/chained/
 
 for ((chain=1;chain<=401;chain=chain+20))
     do
-    base_path=../nets/workflows/chained_workflows_${chain}/
+    base_path=../../instances/chained/chained_${chain}/
 
-    python3 chain_nets.py ../nets/workflows/fc-safety ../nets/workflows/chained_workflows_unreduced_${chain}/ $REPEATS $chain
+    python3 chain_nets.py ../../instances/safe-nets ../../instances/chained/chained_unreduced_${chain}/ $REPEATS $chain
 
-    mkdir -p ../nets/workflows/chained_workflows_templates_${chain}/
-    cp ../nets/workflows/chained_workflows_unreduced_${chain}/* ../nets/workflows/chained_workflows_templates_${chain}/
+    mkdir -p ../../instances/chained/chained_templates_${chain}/
+    cp ../../instances/chained/chained_unreduced_${chain}/* ../../instances/chained/chained_templates_${chain}/
     mkdir -p "${base_path}woflan/"
     mkdir -p "${base_path}lola/"
     mkdir -p "${base_path}continuous/"
 
-    for FILE in ../nets/workflows/chained_workflows_templates_${chain}/*.lola;
+    for FILE in ../../instances/chained/chained_templates_${chain}/*.lola;
         do
         echo ${FILE}
         python3 reduce_net.py ${FILE}
@@ -24,9 +24,9 @@ for ((chain=1;chain<=401;chain=chain+20))
         FILENAME=${FILE##*/}
         FILENAME_NO_EXTENSION=${FILENAME%.lola}
 
-        dotnet ../fastforward/fastforward.dll translate-wf ${FILE} -f PNML -o "${base_path}woflan/${FILENAME_NO_EXTENSION}" -m Reachability
-        dotnet ../fastforward/fastforward.dll translate-wf ${FILE} -f Lola -o "${base_path}lola/${FILENAME_NO_EXTENSION}" -m Soundness
-        dotnet ../fastforward/fastforward.dll translate-wf ${FILE} -f Lola -o "${base_path}continuous/${FILENAME_NO_EXTENSION}" -m Soundness
+        dotnet ../../tools/fastforward/fastforward.dll translate-wf ${FILE} -f PNML -o "${base_path}woflan/${FILENAME_NO_EXTENSION}" -m Reachability
+        dotnet ../../tools/fastforward/fastforward.dll translate-wf ${FILE} -f Lola -o "${base_path}lola/${FILENAME_NO_EXTENSION}" -m Soundness
+        dotnet ../../tools/fastforward/fastforward.dll translate-wf ${FILE} -f Lola -o "${base_path}continuous/${FILENAME_NO_EXTENSION}" -m Soundness
 
         done
     done
