85f5d3b6504bdc17cbbabded91f878fdfcab47c2
Philip Offtermatt <philip.offtermatt@usherbrooke.ca>
2022-02-23 12:54:09 -0500

    Start cleaning up files

--- artifact/benchmark/generate_reachability_instances.sh
+++
@@ -1,3 +0,0 @@
-rm nets/generated_reachability_instances/*
-mkdir -p nets/generated_reachability_instances
-python3 generate_instances.py -o nets/generated_reachability_instances/ nets/largest_instances/*.lola -m Reachability -f Lola --prune
--- artifact/src/republish_with_gurobi.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-rm -R ../benchmark/fastforward/
-GUROBI=true dotnet publish -r linux-x64 -c Release
-mv bin/Release/netcoreapp3.1/linux-x64/publish ../benchmark/fastforward
