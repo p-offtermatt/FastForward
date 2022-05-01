3937fa09617e244858376d01ae1216715ec89b0d
Philip Offtermatt <p.offtermatt@gmail.com>
2022-05-01 14:17:21 +0200

    Install dotnet via snap

--- install.sh
+++ install.sh
@@ -1,14 +1,7 @@
 #!/bin/bash
 
 # install dotnet
-wget https://packages.microsoft.com/config/ubuntu/21.04/packages-microsoft-prod.deb -O packages-microsoft-prod.deb
-sudo dpkg -i packages-microsoft-prod.deb
-rm packages-microsoft-prod.deb
-
-sudo apt-get update; \
-  sudo apt-get install -y apt-transport-https && \
-  sudo apt-get update && \
-  sudo apt-get install -y dotnet-sdk-6.0
+sudo snap install dotnet-sdk --classic --channel=6.0
 
 # installing psutil
 apt-get install python3
