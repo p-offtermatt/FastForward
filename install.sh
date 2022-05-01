0bc1aa7be3cb7d3c6d7b5f84bd5e1444a44ee40b
Philip Offtermatt <p.offtermatt@gmail.com>
2022-05-01 13:48:13 +0200

    Add dotnet to install.sh

--- install.sh
+++ install.sh
@@ -1,7 +1,19 @@
 #!/bin/bash
 
-# installing psutils
-apt-get install psutils
+# install dotnet
+wget https://packages.microsoft.com/config/ubuntu/21.04/packages-microsoft-prod.deb -O packages-microsoft-prod.deb
+sudo dpkg -i packages-microsoft-prod.deb
+rm packages-microsoft-prod.deb
+
+sudo apt-get update; \
+  sudo apt-get install -y apt-transport-https && \
+  sudo apt-get update && \
+  sudo apt-get install -y dotnet-sdk-6.0
+
+# installing psutil
+apt-get install python3
+apt-get install python3-pip
+pip install psutil
 
 # adding nuget sources
 dotnet nuget add source --name nuget.org https://api.nuget.org/v3/index.json
