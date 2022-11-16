3cac2af571cc2367bda04d516379ad84e890c2e6
Philip Offtermatt <philip.offtermatt@usherbrooke.ca>
2022-11-16 10:25:44 -0500

    Adjust scripts to summarize bounded runs and add reduced nets

--- artifact/benchmark/compute_net_statistics_wfs.sh
+++ artifact/benchmark/compute_net_statistics_wfs.sh
@@ -1,9 +1,9 @@
 mkdir -p statistics
 echo "Computing statistics..."
-python3 compute_net_statistics.py nets/workflows/workflows_original/IBM -o statistics/IBM_original.json
-python3 compute_net_statistics.py nets/workflows/workflows_original/SAP -o statistics/SAP_original.json
-
 python3 compute_net_statistics.py nets/workflows/workflows_transformed/IBM -o statistics/IBM_transformed.json
 python3 compute_net_statistics.py nets/workflows/workflows_transformed/SAP -o statistics/SAP_transformed.json
+
+python3 compute_net_statistics.py nets/workflows/workflows_reduced/IBM -o statistics/IBM_reduced.json
+python3 compute_net_statistics.py nets/workflows/workflows_reduced/SAP -o statistics/SAP_reduced.json
 
 echo "Done"
--- artifact/benchmark/summarize_boundedruns.py
+++ artifact/benchmark/summarize_boundedruns.py
@@ -3,28 +3,71 @@ import plotting_utils as utils
 import numpy
 import pandas
 
-def print_statistics(entries):
-    print("Printing statistics...")
-    bounded_runs = [entry["hasBoundedRuns"]for entry in entries if "error" not in entry]
-    bounded_runs_true = [entry for entry in bounded_runs if entry]
-    bounded_runs_false = [entry for entry in bounded_runs if not entry]
+def remove_usual_file_endings(input: str):
+    while True:
+        if input.endswith(".xml"):
+            input = input[:-(len(".xml"))]
+        elif input.endswith(".lola"):
+            input = input[:-(len(".lola"))]
+        elif input.endswith(".tpn"):
+            input = input[:-(len(".tpn"))]
+        else:
+            return input
 
-    print("Total: " + str(len(entries)))
-    print("Have bounded runs: " + str(len(bounded_runs_true)))
-    print("Have unbounded runs: " + str(len(bounded_runs_false)))
+
+def print_statistics(original_entries, transformed_entries):
+    print("Printing statistics...")
+
+    names_to_entry_pairs = dict()
+
+    for entry in original_entries:
+        name = remove_usual_file_endings(entry["sampleName"])
+        matching_entries = [trans_entry  for trans_entry in transformed_entries if remove_usual_file_endings(trans_entry["sampleName"]) == name]
+        if len(matching_entries) != 1 and len(matching_entries) != 0:
+            raise Exception(f"For entry {name}, have {len(matching_entries)} entries that match")
+
+    bounded_before = [entry for entry in original_entries if "error" not in entry and entry["hasBoundedRuns"]]
+    unbounded_before = [entry for entry in original_entries if "error" not in entry and not entry["hasBoundedRuns"]]
+
+    bounded_after = [entry for entry in transformed_entries if "error" not in entry and entry["hasBoundedRuns"]]
+    unbounded_after = [entry for entry in transformed_entries if "error" not in entry and not entry["hasBoundedRuns"]]
+
+
+    print("Total before: " + str(len(original_entries)))
+    print("Total after: " + str(len(transformed_entries)))
+    print("Number of places before: ")
+    df = pandas.Series([entry["places"] for entry in original_entries])
+    print(df.describe())
+    print("Number of places after: ")
+    df = pandas.Series([entry["places"] for entry in transformed_entries])
+    print(df.describe())
+    print("Bounded before: " + str(len(bounded_before)))
+    print("Bounded after: " + str(len(bounded_after)))
+    print("Unbounded before: " + str(len(unbounded_before)))
+    print("Unbounded after: " + str(len(unbounded_after)))
+
+
+    print("Bounded after, free choice: " + str(len([entry for entry in bounded_after if entry["isFreeChoice"]])))
+    print("Unounded after, free choice: " + str(len([entry for entry in unbounded_after if entry["isFreeChoice"]])))
+
+
+
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument('datafiles', nargs="+")
+    parser.add_argument('-o', '--originalfiles', nargs="+", required=True)
+    parser.add_argument('-t', '--transformedfiles', nargs="+", required=True)
 
     args = parser.parse_args()
 
-    collected_data = None
-
-    json_obj = []
+    original_entries = []
+    transformed_entries = []
 
     # read input
-    for filepath in args.datafiles:
-        json_obj += utils.read_json_from_file(filepath)
-    
-    print_statistics(json_obj)
+    for filepath in args.originalfiles:
+        original_entries += utils.read_json_from_file(filepath)
+
+    for filepath in args.transformedfiles:
+        transformed_entries += utils.read_json_from_file(filepath)
+
+    print_statistics(original_entries, transformed_entries)
