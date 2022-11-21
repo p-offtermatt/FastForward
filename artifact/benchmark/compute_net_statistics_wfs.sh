f9d3886bbdf40be2f1220b44da6a83a3576c6cb8
Philip Offtermatt <philip.offtermatt@usherbrooke.ca>
2022-11-21 06:01:11 -0500

    Adjust net statistics script to do deadlock checking

--- artifact/benchmark/compute_net_statistics_wfs.sh
+++ artifact/benchmark/compute_net_statistics_wfs.sh
@@ -1,9 +1,9 @@
 mkdir -p statistics
 echo "Computing statistics..."
-python3 compute_net_statistics.py nets/workflows/workflows_transformed/IBM -o statistics/IBM_transformed.json
-python3 compute_net_statistics.py nets/workflows/workflows_transformed/SAP -o statistics/SAP_transformed.json
+python3 compute_net_statistics.py nets/workflows/workflows_transformed/IBM -o statistics/IBM_transformed.json -args="--int-deadlock --cont-deadlock"
+python3 compute_net_statistics.py nets/workflows/workflows_transformed/SAP -o statistics/SAP_transformed.json -args="--int-deadlock --cont-deadlock"
 
-python3 compute_net_statistics.py nets/workflows/workflows_reduced/IBM -o statistics/IBM_reduced.json
-python3 compute_net_statistics.py nets/workflows/workflows_reduced/SAP -o statistics/SAP_reduced.json
+python3 compute_net_statistics.py nets/workflows/workflows_reduced/IBM -o statistics/IBM_reduced.json -args="--int-deadlock --cont-deadlock"
+python3 compute_net_statistics.py nets/workflows/workflows_reduced/SAP -o statistics/SAP_reduced.json -args="--int-deadlock --cont-deadlock"
 
 echo "Done"
