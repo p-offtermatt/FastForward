392a69fc0189242e3e7ed6d39fcab2abbed4bbc5
Philip Offtermatt <philip.offtermatt@usherbrooke.ca>
2023-01-26 10:17:58 -0500

    Adjust options

--- artifact/benchmark/compute_net_statistics_wfs.sh
+++ artifact/benchmark/compute_net_statistics_wfs.sh
@@ -1,9 +1,9 @@
 mkdir -p statistics
 echo "Computing statistics..."
-python3 compute_net_statistics.py nets/workflows/workflows_transformed/IBM -o statistics/IBM_transformed.json -args="--transition-bottlenecks"
-python3 compute_net_statistics.py nets/workflows/workflows_transformed/SAP -o statistics/SAP_transformed.json -args="--transition-bottlenecks"
+python3 compute_net_statistics.py nets/workflows/workflows_transformed/IBM -o statistics/IBM_transformed.json -args="--cont-sound --check-small-bound-properties"
+python3 compute_net_statistics.py nets/workflows/workflows_transformed/SAP -o statistics/SAP_transformed.json -args="--cont-sound --check-small-bound-properties"
 
-python3 compute_net_statistics.py nets/workflows/workflows_reduced/IBM -o statistics/IBM_reduced.json -args="--transition-bottlenecks"
-python3 compute_net_statistics.py nets/workflows/workflows_reduced/SAP -o statistics/SAP_reduced.json -args="--transition-bottlenecks"
+python3 compute_net_statistics.py nets/workflows/workflows_reduced/IBM -o statistics/IBM_reduced.json -args="--cont-sound --check-small-bound-properties"
+python3 compute_net_statistics.py nets/workflows/workflows_reduced/SAP -o statistics/SAP_reduced.json -args="--cont-sound --check-small-bound-properties"
 
 echo "Done"
